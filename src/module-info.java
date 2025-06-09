69f17019fa982a8ad210fcf12ca5a44e14ca0bf3
jasenfranklin <jbfranklin@arizona.edu>
2025-06-08 17:22:35 -0700

    Delete src directory

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module TP1{
-    requires javafx.controls;
-    requires java.sql;
-    requires javafx.graphics;
-    exports passwordEvaluationTestbed;
-    exports questionAndAnswer;
-    
-    opens applicationMainMethodClasses to javafx.graphics, javafx.fxml;
-}
--- src/questionAndAnswer/override.java
+++
@@ -1,5 +0,0 @@
-package questionAndAnswer;
-
-public @interface override {
-
-}
