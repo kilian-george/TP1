e5c8be44ca4c4336b5b727125c4342eed03115b9
Rgsanta1 <rgsanta1@asu.edu>
2025-06-15 12:20:28 -0700

    Update GUIAddRemoveRolesPage.java

--- src/guiPageClasses/GUIAddRemoveRolesPage.java
+++ src/guiPageClasses/GUIAddRemoveRolesPage.java
@@ -111,8 +111,8 @@ public class GUIAddRemoveRolesPage {
 		theRootPane = theRoot;
 		theDatabase = database;
 		theUser = user;
-		
-		primaryStage.setTitle("CSE 360 Foundation Code: Admin Opertaions Page");
+		//Just a small typo here I found while working on the admin functions
+		primaryStage.setTitle("CSE 360 Foundation Code: Admin Operations Page");
 	
 		double WINDOW_WIDTH = FCMainClass.WINDOW_WIDTH;		
 
@@ -358,7 +358,8 @@ public class GUIAddRemoveRolesPage {
 		if (theRemoveRole.compareTo("<Select a role>") != 0) {
 			if (theDatabase.updateUserRole(theSelectedUser, theRemoveRole, "false") ) {
 				combobox_SelectRoleToRemove = new ComboBox <String>();
-				combobox_SelectRoleToRemove.setItems(FXCollections.observableArrayList(addList));
+				//minor typo here on the next line. It had add instead of remove
+				combobox_SelectRoleToRemove.setItems(FXCollections.observableArrayList(removeList));
 				combobox_SelectRoleToRemove.getSelectionModel().clearAndSelect(0);		
 				setupSelectedUser();
 			}				
