b33f1c2a37dc94ea81843aa2f79252bd12ce20e9
Rgsanta1 <rgsanta1@asu.edu>
2025-07-06 15:29:06 -0700

    Update QuestionGuiTest.java

--- src/questionAndAnswer/QuestionGuiTest.java
+++ src/questionAndAnswer/QuestionGuiTest.java
@@ -1,5 +1,7 @@
 package questionAndAnswer;
 
+import java.util.function.Consumer;
+
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -7,20 +9,28 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class QuestionGuiTest extends Application {
-
+private static QuestionList sharedQuestions;
+private static Consumer<Parent> sharedSwitch;
+public static void setData(QuestionList list, Consumer<Parent> switchView) {
+	sharedQuestions = list;
+	sharedSwitch = switchView;
+}
     @Override
-    public void start(Stage stage) {
-        QuestionGuiStandalone gui = new QuestionGuiStandalone(); 
-        
+    public void start(Stage stage) {        
         
         StackPane root = new StackPane();
-        
-        Scene scene = new Scene(root, 600, 400);
-        
-        gui.setOnViewSwitch(newview ->root.getChildren().setAll(newview));
-        Parent initalView = gui.getView();
-        root.getChildren().add(initalView);
-        stage.setTitle("Test: Question Viewer");
+        Consumer<Parent> switchView = newView ->root.getChildren().setAll(newView);
+        if(sharedQuestions == null) {
+        	sharedQuestions = new QuestionList();
+        }
+        if(sharedSwitch == null) {
+        	sharedSwitch = switchView;
+        }
+        QuestionGuiStandalone gui = new QuestionGuiStandalone(sharedQuestions, sharedSwitch);
+        Parent view = gui.getView();
+        root.getChildren().add(view);
+        Scene scene = new Scene(root, 700, 700);
+        stage.setTitle("question qui testing");
         stage.setScene(scene);
         stage.show();
     }
