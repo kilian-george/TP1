c5bb71ac74acf95b4ed2f3c37e9595f210a93007
Rgsanta1 <rgsanta1@asu.edu>
2025-07-06 15:27:40 -0700

    Update Comment.java

--- src/questionAndAnswer/Comment.java
+++ src/questionAndAnswer/Comment.java
@@ -1,23 +1,38 @@
 package questionAndAnswer;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public class Comment implements Serializable{
-/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+public class Comment{
+
 private String text;
 private LocalDateTime timestamp;
-public Comment(String text) {
+private String name;
+public Comment(String name, String text) {
+	this.name= name;
 	this.text = text;
 	this.timestamp = LocalDateTime.now();
+}
+
+public String getName() {
+	return name;
 }
 	public String getText() {
 		return text;
 	}
-	public LocalDateTime gettimestamp() {
+	public LocalDateTime getTimestamp() {
 		return timestamp;
+	}
+
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void setId(int int1) {
+		// TODO Auto-generated method stub
+	}
+
+	public void setTimestamp(LocalDateTime timestamp2) {
+		// TODO Auto-generated method stub	
 	}
 }
