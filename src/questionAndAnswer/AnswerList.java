0b811fb61e2fb45108153d2e571bd1e8c62e22a0
Rgsanta1 <rgsanta1@asu.edu>
2025-07-06 15:27:06 -0700

    Update AnswerList.java

--- src/questionAndAnswer/AnswerList.java
+++ src/questionAndAnswer/AnswerList.java
@@ -1,13 +1,12 @@
 package questionAndAnswer;
 
-import java.io.Serializable;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class AnswerList implements Serializable{
+public class AnswerList{
 	//creates a list of answers to fill out the text that jfx will pull from 
-	// TODO store the list permanently on initial startup onto something external for now
 	private List<Answer> answerList = new ArrayList<>();
 	public void addAnswer(Answer ans) {
 		answerList.add(ans);
@@ -17,5 +16,8 @@ public class AnswerList implements Serializable{
 	}
 	public List<Answer> getAnswers(String questionName){
 		return answerList.stream().filter(a->a.getName().equals(questionName)).collect(Collectors.toList());
+	}
+	public List<Answer> getResolvedAnswers(){
+		return answerList.stream().filter(Answer::getResolved).collect(Collectors.toList());
 	}
 }
